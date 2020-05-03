b97fd5feaf955f833689d971ab2c0f16673d3708
Artsiom <sorintema@gmail.com>
2020-05-03 14:26:21 +0300

    tags models

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
-    'search'
+    'search',
+    'tags',
 ]
 
 MIDDLEWARE = [
--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.urls import reverse
-from .utils import unique_slug_generator
 from django.db.models.signals import pre_save, post_save
 from django.db.models import Q
-import os, random
 
+import os, random
+from .utils import unique_slug_generator
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
