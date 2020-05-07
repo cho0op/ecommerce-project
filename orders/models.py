04bf9ee63bd4be641aaf0781af01d763e0a8ecba
Artsiom <sorintema@gmail.com>
2020-05-07 13:44:00 +0300

    order model

---
+++ orders/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from carts.models import Cart
+
+class Order(models.Model):
+    order_id=models.CharField(max_length=120)
+    #billing_profile
+    #shipping_address
+    #billing_address
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status = models.CharField(max_length=120, default='created')
