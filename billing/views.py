37134724536810414d7fff1a9135bee6f44bee88
Artsiom <sorintema@gmail.com>
2020-06-03 17:11:18 +0300

    stripe payment form

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import stripe
+
+stripe.api_key = "sk_test_uAMQwOWADa01RqYBsCucGkhF00rOxC8nDa"
+STRIPE_PUB_KEY = "pk_test_1qDV1Bm7yRQqOGJhUwWdVN9F001dQvT4Ir"
+
+
+def payment_method_post(request):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
 
 # Create your views here.
