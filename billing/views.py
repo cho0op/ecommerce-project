5fe538d144db0a8828af5b9acc7ece4a96f7b46d
Artsiom <sorintema@gmail.com>
2020-06-06 16:29:55 +0300

    fixed stripe form

--- billing/views.py
+++ billing/views.py
@@ -8,14 +8,15 @@ STRIPE_PUB_KEY = "pk_test_1qDV1Bm7yRQqOGJhUwWdVN9F001dQvT4Ir"
 
 
 def payment_method_post(request):
-    next_url=None
+    next_url = None
     next_ = request.GET.get('next')
     if is_safe_url(next_, request.get_host()):
-        next_url=next_
-    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url":next_url})
+        next_url = next_
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
 
 
 def payment_method_create_view(request):
+
     if request.method == "POST" and request.is_ajax():
         print(request.POST)
         return JsonResponse({"message": "done"})
