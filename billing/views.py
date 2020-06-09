19637df9fca6b290dbe0109928d8a32de698f422
Artsiom <sorintema@gmail.com>
2020-06-09 14:55:54 +0300

    customer in stripe

--- billing/views.py
+++ billing/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
+from billing.models import BillingProfile
 import stripe
 
 stripe.api_key = "sk_test_uAMQwOWADa01RqYBsCucGkhF00rOxC8nDa"
@@ -8,6 +9,9 @@ STRIPE_PUB_KEY = "pk_test_1qDV1Bm7yRQqOGJhUwWdVN9F001dQvT4Ir"
 
 
 def payment_method_view(request):
+    billing_profile, billing_profile_created=BillingProfile.objects.new_or_ger(request)
+    if not billing_profile:
+        return redirect("carts:home")
     next_url = None
     next_ = request.GET.get('next')
     if is_safe_url(next_, request.get_host()):
@@ -16,9 +20,15 @@ def payment_method_view(request):
 
 
 def payment_method_create_view(request):
-
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_ger(request)
+    if not billing_profile:
+        return HttpResponse({'message':"cant find user"}, status_code=401)
+    token = request.POST.get('token')
+    if token is not None:
+        customer=stripe.Customer.retrieve(billing_profile.customer_id)
+        card_response=customer.sources.create(source=token)
+        print(card_response)
     if request.method == "POST" and request.is_ajax():
-        print(request.POST)
         return JsonResponse({"message": "done"})
     return HttpResponse("error", status=401)
 # Create your views here.
